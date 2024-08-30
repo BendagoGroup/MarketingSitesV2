0f14c98aa5b46cd21657aade445173f5b7162572
Ido <hilloy15@gmail.com>
2024-08-30 17:05:11 -0400

    fixing some other stuff i hope 4

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -44,7 +44,7 @@ export const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <a href="" className="hover:underline">
+          <a href="/" className="hover:underline">
             BENDAGO
           </a>
           . All Rights Reserved.
